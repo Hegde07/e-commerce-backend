680e0b3d238f84a42c10f629d640202fe848d8f6
Hegde07 <hegdebhargav92@gmail.com>
2023-08-26 11:51:22 -0700

    first commit

---
+++ routes/Products.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const {createProduct} = require('../controller/Product')
+
+
+const router = express.Router();
+
+router.post('/',createProduct);
+
+exports.router = router;
